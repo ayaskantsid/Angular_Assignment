93fd5d5fd3b515372da6d7852686d33b1666880c
Ayaskant Siddharth <ishugzb1998@gmail.com>
2020-04-21 20:31:55 +0530

    completed assignment_2

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AssignmentComponent, Todo } from '../../assignment.component';
 
 @Component({
   selector: 'app-assignment2',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment2.component.css']
 })
 export class Assignment2Component {
+  
+  todoList:Todo[]
+  constructor(private assignmentComponent:AssignmentComponent){}
+
+  ngOnInit(){
+    this.todoList = this.assignmentComponent.todos
+    console.log(this.todoList)
+  }
+
+  AddClass(event){
+    event.target.parentNode.className = "active"
+  }
+
+  CreateTableContent(data:string) : HTMLTableDataCellElement{
+    let tag = document.createElement('td')
+    tag.textContent = data
+    return tag
+  }
 
 }
